9fa08e5bd02ef06d9239ed4c9a7001e37baf5f21
Shady Boshra <shady_alkrar@yahoo.com>
2019-11-05 12:45:22 +0200

    Initial Commit

---
+++ app/src/main/java/com/shadyboshra2012/android/udacitysinglescreenproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shadyboshra2012.android.udacitysinglescreenproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
